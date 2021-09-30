e825d327b9da1a0e15785ba76c5e20a66781e559
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-30 04:49:20 +0200

    Update and rename vnc_startup.sh to vnc_0_startup.sh

--- starting/vnc_0_startup.sh
+++ starting/vnc_0_startup.sh
@@ -2,21 +2,21 @@
 export PASSWORD="123123123"
 echo "000000000000000000000000000000000000001111111111111111111111111111111111111"
 mkdir -p /root/.vnc/
-echo $PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+echo $PASSWORD | vncpasswd -f > /root/.vnc/passwd
 chmod 600 $HOME/.vnc/passwd
 
 
 vncserver
 vncserver -kill :1
 mv ~/.vnc/xstartup ~/.vnc/xstartup.bak
-cat <<EOF > $HOME/.vnc/xstartup
+cat <<EOF > /root/.vnc/xstartup
 #!/bin/bash
-xrdb $HOME/.Xresources
+xrdb /root/.Xresources
 startxfce4 &
 EOF
-chmod +x $HOME/.vnc/xstartup
+chmod +x /root/.vnc/xstartup
 
-touch $HOME/.Xresources
+touch /root/.Xresources
 vncserver
 #websockify -D --web=/usr/share/novnc/  6080 localhost:5901
 
