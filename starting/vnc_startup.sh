6f0ffa1c6a02984b050525f462d5a9a01afa711c
y0shimitsugh0st84 <68965220+y0shimitsugh0st84@users.noreply.github.com>
2021-09-30 03:38:49 +0200

    Update vnc_startup.sh

--- starting/vnc_startup.sh
+++ starting/vnc_startup.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+export PASSWORD="123123123"
+echo "000000000000000000000000000000000000001111111111111111111111111111111111111"
+mkdir -p /root/.vnc/
+echo $PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
+chmod 600 $HOME/.vnc/passwd
+cat <<EOF > $HOME/.vnc/xstartup
+#!/bin/bash
+xrdb $HOME/.Xresources
+startxfce4 &
+EOF
+chmod +x $HOME/.vnc/xstartup
+
 
